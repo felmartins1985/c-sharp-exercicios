d656af6a41550ff839d346eb2cb2fa60a0c517e0
hp <hp@pop-os.localdomain>
2023-02-27 15:32:01 -0300

    feat: exercicios 1 e 2

---
+++ secao2/exercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     int n1= int.Parse(Console.ReadLine());
+     int n2= int.Parse(Console.ReadLine());
+     Console.WriteLine("SOMA = " + (n1+n2));
+    }
+  }
+}
---
+++ secao2/exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double pi = 3.14159;
+      double area = pi * (raio * raio);
+      Console.WriteLine("A= " + area.ToString("F4", CultureInfo.InvariantCulture));
+    }
+  }
+}
