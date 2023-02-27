acb291ec87ceeae0f9d9de2d00810d9a1da6408e
hp <hp@pop-os.localdomain>
2023-02-27 15:48:23 -0300

    feat: exercicios 3 e 4

---
+++ secao2/exercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int A= int.Parse(Console.ReadLine());
+      int B= int.Parse(Console.ReadLine());
+      int C= int.Parse(Console.ReadLine());
+      int D= int.Parse(Console.ReadLine());
+      int diferenca = (A * B - C * D);
+      Console.WriteLine("DIFERENCA = " + diferenca);
+    }
+  }
+}
---
+++ secao2/exercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = int.Parse(Console.ReadLine());
+      int hours = int.Parse(Console.ReadLine());
+      double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double salary = hours * valuePerHour;
+      Console.WriteLine("NUMBER = " + number);
+      Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+    }
+  }
+}
