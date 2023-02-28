cf72760fc15596f5a61864b3435c753bdc31bdf1
hp <hp@pop-os.localdomain>
2023-02-28 11:07:30 -0300

    feat: exemplo 27

---
+++ secao2/exemplo_parte27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Qual a hora atual?");
+      int hora = int.Parse(Console.ReadLine());
+      if (hora < 12)
+      {
+        Console.WriteLine("Bom dia!");
+      }
+      else if (hora < 18)
+      {
+        Console.WriteLine("Boa tarde!");
+      }
+      else
+      {
+        Console.WriteLine("Boa noite!");
+      }
+    }
+  }
+}
+
