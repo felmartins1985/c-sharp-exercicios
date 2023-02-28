856949cf1fab0bc384ddd1efc8084bd7e5231638
hp <hp@pop-os.localdomain>
2023-02-28 14:26:28 -0300

    feat: exercicio 2.4

---
+++ secao2/exercicio2.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Digite a hora inicial do jogo:");
+      int horaInicial = int.Parse(Console.ReadLine());
+      Console.WriteLine("Digite a hora final do jogo:");
+      int horaFinal = int.Parse(Console.ReadLine());
+      int duracao = 0;
+      if (horaInicial < horaFinal)
+      {
+        duracao = horaFinal - horaInicial;
+      }
+      else
+      {
+        duracao = 24 - horaInicial + horaFinal;
+      }
+      Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+    }
+
+  }
+}
+
