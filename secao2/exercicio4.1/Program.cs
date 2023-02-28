12ee68808cb65dd44dfe62fc894e5c580dfd18e2
hp <hp@pop-os.localdomain>
2023-02-28 17:56:18 -0300

    feat: exercicio 4.1

---
+++ secao2/exercicio4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite um numero ");
+      int N = int.Parse(Console.ReadLine());
+      for (int i = 1; i <= N && i <= 1000; i++)
+      {
+        if (i % 2 != 0)
+        {
+          System.Console.WriteLine(i);
+        }
+      }
+    }
+  }
+}
+
