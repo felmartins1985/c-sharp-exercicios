0b2913f17f4a004f039e0996732e7b59897c059f
hp <hp@pop-os.localdomain>
2023-02-28 17:10:24 -0300

    feat: exercicio 3.3

---
+++ secao2/exercicio3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite como deseja abastecer: ");
+      int alcool = 0;
+      int gasolina = 0;
+      int diesel = 0;
+      int combustivel = int.Parse(Console.ReadLine());
+      while (combustivel != 4)
+      {
+        if (combustivel == 1)
+        {
+          alcool += 1;
+        }
+        else if (combustivel == 2)
+        {
+          gasolina += 1;
+        }
+        else if (combustivel == 3)
+        {
+          diesel += 1;
+        }
+        else
+        {
+          System.Console.WriteLine("Código inválido: digite novamente");
+        }
+        combustivel = int.Parse(Console.ReadLine());
+
+      }
+      Console.WriteLine("MUITO OBRIGADO");
+      Console.WriteLine("Alcool: " + alcool);
+      Console.WriteLine("Gasolina: " + gasolina);
+      Console.WriteLine("Diesel: " + diesel);
+
+    }
+  }
+}
