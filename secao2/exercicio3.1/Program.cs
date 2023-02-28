5070e498bf24a10e3ee1d41d819fcb11a4ff51b8
hp <hp@pop-os.localdomain>
2023-02-28 16:35:54 -0300

    feat: exercicio 3.1

--- secao2/exercicio3.1/Program.cs
+++ secao2/exercicio3.1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite a senha correta: ");
+      int password = int.Parse(Console.ReadLine());
+      while (password != 2002)
+      {
+        System.Console.WriteLine("Senha incorreta");
+        Console.Write("Digite a senha correta: ");
+        password = int.Parse(Console.ReadLine());
+      }
+      System.Console.WriteLine("Acesso permitido");
+    }
+  }
+}
