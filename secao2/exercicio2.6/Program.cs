5ea5e33ff7463c6ce8ff9fbebe9c64f9646951c1
hp <hp@pop-os.localdomain>
2023-02-28 14:43:23 -0300

    feat: exercicio 2.6

---
+++ secao2/exercicio2.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Digite um numero: ");
+      double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      if (numero >= 0 && numero <= 25)
+      {
+        Console.WriteLine("Intervalo [0,25]");
+      }
+      else if (numero >25 &&  numero <= 50 ){
+        Console.WriteLine("Intervalo (25,50]");
+      }
+      else if (numero > 50 && numero <= 75)
+      {
+        Console.WriteLine("Intervalo (50,75]");
+      }
+      else if (numero > 75 && numero <= 100)
+      {
+        Console.WriteLine("Intervalo (75,100]");
+      }
+      else
+      {
+        Console.WriteLine("Fora de intervalo");
+      }
+
+    }
+
+  }
+}
+
