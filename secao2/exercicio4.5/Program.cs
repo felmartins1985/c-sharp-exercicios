eba6309b2ecd0f0755202f54080d92c215c185bb
hp <hp@pop-os.localdomain>
2023-02-28 18:36:40 -0300

    feat: exercicio 4.5

---
+++ secao2/exercicio4.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite um numero ");
+      int n = int.Parse(Console.ReadLine());
+      int soma = 1;
+      for (int i= n; i > 0; i--)
+      {
+        soma *= i;
+      }
+      System.Console.WriteLine($"O valor fatorial foi {soma}");
+
+      }
+  }
+}
