28687fcd1b89bb52689c0d5c44952968ebf00c16
hp <hp@pop-os.localdomain>
2023-02-28 11:40:34 -0300

    feat: exercicio 2.2

---
+++ secao2/exercicio2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization; // necessario para formatar a saida de dados, colocando o ponto
+// ao inves de virgulas para numeros decimais --> CultureInfo.InvariantCulture
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Digite um numero: ");
+      int numero = int.Parse(Console.ReadLine());
+      if (numero % 2 == 0)
+      {
+        Console.WriteLine("PAR");
+      }
+      else
+      {
+        Console.WriteLine("IMPAR");
+      }
+
+
+    }
+  }
+}
+
