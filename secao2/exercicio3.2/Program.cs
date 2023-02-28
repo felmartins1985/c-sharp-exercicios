c87447e93fe8b89241e821a86dbce9869dac6c98
hp <hp@pop-os.localdomain>
2023-02-28 16:45:55 -0300

    feat: exercicio 3.2

---
+++ secao2/exercicio3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite a coordenada x: ");
+      int x = int.Parse(Console.ReadLine());
+      Console.Write("Digite a coordenada y: ");
+      int y = int.Parse(Console.ReadLine());
+      while (x != 0 && y!=0){
+        if (x>0 && y>0){
+          System.Console.WriteLine("Primeiro");   
+        }
+        else if (x >0 && y<0){
+          System.Console.WriteLine("Quarto");
+        }
+        else if (x<0 && y>0){
+          System.Console.WriteLine("Segundo");
+        }
+        else {
+          System.Console.WriteLine("Terceiro");
+        }
+        System.Console.WriteLine("Digite a coordenada x: ");
+        x = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Digite a coordenada y: ");
+        y = int.Parse(Console.ReadLine());
+      }
+
+    }
+  }
+}
