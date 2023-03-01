a95dced12f705111d47bb65477e5236f174f6837
hp <hp@pop-os.localdomain>
2023-03-01 13:54:38 -0300

    feat: exercicio 4.6

---
+++ secao2/exercicio4.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite um numero ");
+      int n = int.Parse(Console.ReadLine());
+      for (int i = 1; i <= n; i++)
+      {
+        if (n % i == 0)
+        {
+          System.Console.WriteLine($" O {n} e divisivel por {i}");
+
+        }
+      }
+
+    }
+  }
+}
