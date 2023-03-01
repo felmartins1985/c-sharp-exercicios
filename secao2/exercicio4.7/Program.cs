a47e17be7c7bda4829adb055e41565aa4cb1ad65
hp <hp@pop-os.localdomain>
2023-03-01 14:01:01 -0300

    feat: exercicio 4.7

---
+++ secao2/exercicio4.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite um numero ");
+      int n = int.Parse(Console.ReadLine());
+      // int quadrado = 0;
+      // int cubo = 0;
+      for (int i = 1; i <= n; i++)
+      {
+        int quadrado= i * i;
+        int cubo = i * i * i;
+        System.Console.WriteLine($"{i} {quadrado} {cubo}");
+      }
+
+    }
+  }
+}
